dd47072ddf06ab15c0ae53f74a900628afe36793
tsukasa2 <49301252+tsukasa2@users.noreply.github.com>
2021-03-21 14:59:16 +0900

    Delete others directory

--- others/M-SOLUTIONS2020/m-sol20-c.py
+++
@@ -1,8 +0,0 @@
-n, k = map( int, input().split() )
-a = list( map( int, input().split() ) )
-
-for i in range( n - k ):
-    if a[ k + i ] > a[ i ]:
-        print( "Yes" )
-    else:
-        print( "No" )
