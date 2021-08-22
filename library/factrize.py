27a1ab56a518c6c3d15974bec48271a260da066e
tsukasa2 <t.nakamura@posl.ait.kyushu-u.ac.jp>
2021-08-22 16:39:35 +0900

    factrize

--- library/factrize.py
+++ library/factrize.py
@@ -1,20 +1,28 @@
-A, B = map( int, input().split() )
-
-# factorize( 18 ) = [ ( 2, 1 ), ( 3, 2 ) ]
+# factize( 252 ) -> [ ( 2, 2 ), ( 3, 2 ), ( 7, 1 ) ]
 def factorize( n ):
     res = []
-    p = 2
     tmp = n
+    r = 0
+    if tmp % 2 == 0:
+        while tmp % 2 == 0:
+            tmp //= 2
+            r += 1
+        
+        res.append( ( 2, r ) )
+    
+    p = 3
     while p <= n ** 0.5 + 1:
         if tmp % p != 0:
-            p += 1
+            p += 2
             continue
+
         r = 0
         while tmp % p == 0:
             tmp //= p
             r += 1
+
         res.append( ( p, r ) )
-        p += 1
+        p += 2
     
     if tmp != 1:
         res.append( ( tmp, 1 ) )
@@ -24,8 +32,4 @@ def factorize( n ):
 
     return res
 
-import math
-
-g = math.gcd( A, B )
-
-print( 1 + len( factorize( g ) ) )
+print( factorize( 252 ) )
